6b465032899c2123991bb8b5cc5bf375227b7104
mvalleron <maxime.valleron@etu.u-bordeaux.fr>
2017-12-11 21:19:55 +0100

    Update Controller.java

--- Labyrinth/src/Controller/Controller.java
+++ Labyrinth/src/Controller/Controller.java
@@ -27,17 +27,17 @@ public class Controller implements ActionListener
 	private static AnimationTimer timer;
 	private static Point arriver;
 	private static boolean demarrage;
+	private static CarteDistance carte;
+	private static Monstre[] monstres;
 	
+	private static int nbMonstre = 2;
 	private static int tailleLabyrinthe = 10;
-	
-	private int turn = 0;
-	
-	public static int getTurn() {
-		return turn;
-	}
 
 	public static Point GetJoueurPosition() {
 		return joueur.GetPosition();
+	}
+	public static CarteDistance getCarte() {
+		return carte;
 	}
 	
 	public static Controller getInstance() {
@@ -51,8 +51,29 @@ public class Controller implements ActionListener
 		arriver = new Point(tailleLabyrinthe-1,tailleLabyrinthe-1);
 		View.getInstance();
 	    model= new Model();
+	    carte = new CarteDistance();
+	    monstres = new Monstre[nbMonstre];
+	    initMonstre();
 	}
 
+	private static void initMonstre() {
+		int i = 0;
+		while(i < nbMonstre-1) {
+			monstres[i] =  new Monstre((int)(Math.random()*(tailleLabyrinthe-1)),(int)(Math.random()*(tailleLabyrinthe-1)));
+			if(monstres[i].GetPosition().compareTo(joueur.GetPosition())==0)
+				i--;
+			if(monstres[i].GetPosition().compareTo(arriver)==0)
+				i--;
+			i++;
+		}
+		
+	}
+	public static int getNbMonster() {
+		return nbMonstre;
+	}
+	public static Point positionMonstre(int i) {
+		return monstres[i].GetPosition();
+	}
 	public static void start(Stage primaryStage) throws Exception {
 		ViewGame.getInstance().createGlobalView(primaryStage, model.getLabyrinth());
 		ViewGame.getInstance().raffraichir(model.getLabyrinth());
@@ -66,6 +87,8 @@ public class Controller implements ActionListener
 			joueur.SetPosition(new Point(0,0));
 			arriver = new Point(tailleLabyrinthe-1,tailleLabyrinthe-1);
 			model.setLabyrinth(new Graph(tailleLabyrinthe,tailleLabyrinthe));
+			nbMonstre++;
+			initMonstre();
 		}
 		
 	}
@@ -74,23 +97,16 @@ public class Controller implements ActionListener
 				
 			int x =joueur.GetPosition().GetX();
 			int y =joueur.GetPosition().GetY();
-			if(event.getCode().equals(KeyCode.UP)) {
-				turn = 1;
+			if(event.getCode().equals(KeyCode.UP))
 				joueur.SetPosition(Model.getLabyrinth().GetPoint(x, y).GetNord());
-			}
-			else if(event.getCode()==KeyCode.DOWN) {
-				turn = 0;
+			else if(event.getCode()==KeyCode.DOWN)
 				joueur.SetPosition(Model.getLabyrinth().GetPoint(x, y).GetSud());
-			}
-			else if(event.getCode()==KeyCode.LEFT) {
-				turn = 2;
+			else if(event.getCode()==KeyCode.LEFT)
 				joueur.SetPosition(Model.getLabyrinth().GetPoint(x, y).GetOuest());
-			}
-			else if(event.getCode()==KeyCode.RIGHT) {
-				turn = 3;
+			else if(event.getCode()==KeyCode.RIGHT)
 				joueur.SetPosition(Model.getLabyrinth().GetPoint(x, y).GetEst());
-			}
 			GameOver();	
+			carte = new CarteDistance();
 		}	
 	};
 	public void actionPerformed(ActionEvent arg0) {
@@ -101,6 +117,9 @@ public class Controller implements ActionListener
                     	scheduler.shutdown();
                         Platform.runLater(new Runnable(){
                             public void run() {
+                            	for(int i = 0; i<nbMonstre;i++){
+                            		//monstres[i].NextPoint();
+                            	}
                             	ViewGame.getInstance().raffraichir(model.getLabyrinth());
                             }
                         });   
