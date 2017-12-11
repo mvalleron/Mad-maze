987d93f60d567e1803b264d634a6ceefe638c9e6
mvalleron <maxime.valleron@etu.u-bordeaux.fr>
2017-12-11 15:59:40 +0100

    Update Controller.java

--- Labyrinth/src/Controller/Controller.java
+++ Labyrinth/src/Controller/Controller.java
@@ -6,9 +6,13 @@ package Controller;
 import javafx.event.*;
 import javafx.scene.input.*;
 import javafx.stage.Stage;
+import javafx.application.Platform;
+import java.util.concurrent.TimeUnit;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import Model.*;
 import View.*;
@@ -22,6 +26,7 @@ public class Controller implements ActionListener
 	private static Model model;
 	private static AnimationTimer timer;
 	private static Point arriver;
+	private static boolean demarrage;
 	
 	private static int tailleLabyrinthe = 10;
 
@@ -44,13 +49,23 @@ public class Controller implements ActionListener
 
 	public static void start(Stage primaryStage) throws Exception {
 		ViewGame.getInstance().createGlobalView(primaryStage, model.getLabyrinth());
+		ViewGame.getInstance().raffraichir(model.getLabyrinth());
 		primaryStage.show();
 		timer = new AnimationTimer(getInstance());
 		timer.start();
-
+	}
+	private static void GameOver() {
+		if(joueur.GetPosition().compareTo(arriver) == 0) {
+			tailleLabyrinthe++;
+			joueur.SetPosition(new Point(0,0));
+			arriver = new Point(tailleLabyrinthe-1,tailleLabyrinthe-1);
+			model.setLabyrinth(new Graph(tailleLabyrinthe,tailleLabyrinthe));
+		}
+		
 	}
 	public static EventHandler<KeyEvent> eventHandlerButton = new EventHandler<KeyEvent>() {
 		public void handle(KeyEvent event) {
+				
 			int x =joueur.GetPosition().GetX();
 			int y =joueur.GetPosition().GetY();
 			if(event.getCode().equals(KeyCode.UP))
@@ -61,23 +76,22 @@ public class Controller implements ActionListener
 				joueur.SetPosition(Model.getLabyrinth().GetPoint(x, y).GetOuest());
 			else if(event.getCode()==KeyCode.RIGHT)
 				joueur.SetPosition(Model.getLabyrinth().GetPoint(x, y).GetEst());
-			ViewGame.getInstance().raffraichir(model.getLabyrinth());
 			GameOver();	
-		}
-
-		private void GameOver() {
-			if(joueur.GetPosition().compareTo(arriver) == 0) {
-				tailleLabyrinthe++;
-				joueur.SetPosition(new Point(0,0));
-				arriver = new Point(tailleLabyrinthe-1,tailleLabyrinthe-1);
-				model.setLabyrinth(new Graph(tailleLabyrinthe,tailleLabyrinthe));
-				ViewGame.getInstance().raffraichir(model.getLabyrinth());
-			}
-			
 		}	
 	};
 	public void actionPerformed(ActionEvent arg0) {
-		//ViewGame.getInstance().raffraichir(model.getLabyrinth());
+		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+		scheduler.scheduleAtFixedRate(
+				new Runnable(){
+                	public void run() {
+                    	scheduler.shutdown();
+                        Platform.runLater(new Runnable(){
+                            public void run() {
+                            	ViewGame.getInstance().raffraichir(model.getLabyrinth());
+                            }
+                        });   
+                    }
+                },1,1,TimeUnit.MILLISECONDS);	
 	}
 }
 
