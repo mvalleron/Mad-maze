28ff9dba6898e51551cae0d6c2116709f9789b4b
mvalleron <maxime.valleron@etu.u-bordeaux.fr>
2017-12-16 21:23:28 +0100

    Add files via upload

---
+++ Labyrinth/src/Controller/Interrupteur.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import Model.Arrete;
+import Model.Arrete.Matiere;
+import Model.Point;
+
+public class Interrupteur {
+	public enum Action {
+		ouvrir,
+		fermer;
+	}
+	private Point position;
+	private Arrete porte;
+	private Action actionPorte;
+	public Interrupteur(Point position,Arrete porte,Action actionPorte) {
+		this.position = position;
+		this.porte = porte;
+		this.actionPorte = actionPorte;
+	}
+	public Action GetAction() {
+		return actionPorte;
+	}
+	public Point GetPosition() {
+		return position;
+	}
+	public void interrupteurActionner() {
+		if(actionPorte == Action.ouvrir)
+			porte.setType(Matiere.corridor);
+		else
+			porte.setType(Matiere.mur);
+	}
+}
