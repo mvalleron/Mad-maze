058734a5171f44e57943d3c28f0cb0586d481c73
mvalleron <maxime.valleron@etu.u-bordeaux.fr>
2017-12-16 21:24:48 +0100

    Update Monstre.java

--- Labyrinth/src/Controller/Monstre.java
+++ Labyrinth/src/Controller/Monstre.java
@@ -5,8 +5,13 @@ import Model.Point;
 
 public class Monstre {
 	private Point position;
+	private int lenteurMonstre = 0;
+	private int incrementeur = 0;
 	public Monstre(int x,int y) {
 		position = new Point(x,y);
+	}
+	public void SetLenteur(int l) {
+		lenteurMonstre = l;
 	}
 	public void SetPosition(Point p) {
 		if(p != null)
@@ -16,20 +21,21 @@ public class Monstre {
 		return position;
 	}
 	public void NextPoint() {
-		CarteDistance carte = Controller.getCarte();
-		Point selection = Model.Model.getLabyrinth().getPosition(position.GetX(),position.GetY());
-		Point tmp = selection.GetEst();
-		if(tmp != null && carte.tab[tmp.GetX()][tmp.GetY()] < carte.tab[selection.GetX()][selection.GetY()]) 
-			selection = tmp;
-		tmp = selection.GetNord();
-		if(tmp != null && carte.tab[tmp.GetX()][tmp.GetY()] < carte.tab[selection.GetX()][selection.GetY()]) 
-			selection = tmp;
-		tmp = selection.GetOuest();
-		if(tmp != null && carte.tab[tmp.GetX()][tmp.GetY()] < carte.tab[selection.GetX()][selection.GetY()]) 
-			selection = tmp;
-		tmp = selection.GetSud() ;
-		if(tmp != null && carte.tab[tmp.GetX()][tmp.GetY()] < carte.tab[selection.GetX()][selection.GetY()]) 
-			selection = tmp;
-		position = new Point(selection.GetX(),selection.GetY());
+		if(lenteurMonstre <= incrementeur) {
+			CarteDistance carte = Controller.getCarte();
+			Point selection = Model.Model.getLabyrinth().getPosition(position.GetX(),position.GetY());
+			if(selection.GetEst() != null && carte.tab[selection.GetEst().GetX()][selection.GetEst().GetY()] < carte.tab[selection.GetX()][selection.GetY()]) 
+				selection = selection.GetEst();
+			else if(selection.GetNord() != null && carte.tab[selection.GetNord().GetX()][selection.GetNord().GetY()] < carte.tab[selection.GetX()][selection.GetY()]) 
+				selection = selection.GetNord();
+			else if(selection.GetOuest() != null && carte.tab[selection.GetOuest().GetX()][selection.GetOuest().GetY()] < carte.tab[selection.GetX()][selection.GetY()]) 
+				selection = selection.GetOuest();
+			else if(selection.GetSud() != null && carte.tab[selection.GetSud().GetX()][selection.GetSud().GetY()] < carte.tab[selection.GetX()][selection.GetY()]) 
+				selection = selection.GetSud();
+			position = new Point(selection.GetX(),selection.GetY());
+			incrementeur = 0;
+		}
+		else
+			incrementeur++;
 	}
 }
