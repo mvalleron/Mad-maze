5c77d34f788479321063c16268bfdc0f9cb530d9
mvalleron <maxime.valleron@etu.u-bordeaux.fr>
2017-12-21 11:34:23 +0100

    Update Point.java

--- Labyrinth/src/Model/Point.java
+++ Labyrinth/src/Model/Point.java
@@ -31,6 +31,14 @@ public class Point implements Comparable<Point>{
 		return y;
 	}
 	
+	public void SetX(int x) {
+		this.x = x;
+	}
+	
+	public void SetY(int y) {
+		this.y = y;
+	}
+	
 	public void SetSud(Point p){
 		voisin[3] = new Arrete(this,p);
 		if(p != null)
